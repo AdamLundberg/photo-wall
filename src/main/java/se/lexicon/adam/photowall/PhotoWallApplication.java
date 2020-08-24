a7b16ed3967ca8bf5a9d82098625d26a36924d7f
Adam <adamlundberg91@gmail.com>
2020-08-24 12:56:37 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/adam/photowall/PhotoWallApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.adam.photowall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/adam/photowall/PhotoWallApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.adam.photowall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
